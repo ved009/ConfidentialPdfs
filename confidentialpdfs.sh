fcdada9f47298e87f9e6f4841f1e27a5dcfb5770
Vedant Roy <vedantroy42@gmail.com>
2023-12-26 09:38:38 +0530

    Update confidentialpdfs.sh

--- confidentialpdfs.sh
+++ confidentialpdfs.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-cat subdomains.txt | gau --subs --threads 20 | grep -Ea '\\.pdf' | tee pdfs.txt
-cat pdfs.txt | httpx -silent -mc 200 | alivepdfs.txt
+#Collect Pdf Files using Gau
+cat subdomains.txt | gau --subs --threads 20 | grep -Ea '.pdf' | tee pdfs.txt
+
+#Find Alive Pdf Files
+cat pdfs.txt | httpx -silent -mc 200 | tee alivepdfs.txt
 
 #Loop through each line in the alivepdfs.txt file
 while IFS= read -r url; do
-    # Download the PDF file using wget or curl
-    wget "$url" -O temp.pdf || curl -o temp.pdf "$url"
+    # Download the PDF file using wget with defined Timeout
+    wget "$url" -O temp.pdf --no-check-certificate --timeout=10
 
     # Convert the PDF to text
     pdftotext temp.pdf temp.txt
 
-    # Search for the specified strings in the converted text file
-    if grep -qE "internal use only|Confidential" temp.txt; then
+    # Search for the specified strings in the converted text file, can also add more strings to check 
+    if grep -qE "Internal use only|Confidential" temp.txt; then
         # If found, append the URL to the leaks.txt file
         echo "$url" >> leaks.txt
     fi
